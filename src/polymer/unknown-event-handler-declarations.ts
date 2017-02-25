6cbfae6fb6ca23e186ff6affafc3d1cc9cf9d716
43081j <43081j@users.noreply.github.com>
2017-02-25 00:27:42 +0000

    add unknown event declarations rule

---
+++ src/polymer/unknown-event-handler-declarations.ts
@@ -0,0 +1,74 @@
+/**
+ * @license
+ * Copyright (c) 2017 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at
+ * http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at
+ * http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+
+import {Document} from 'polymer-analyzer/lib/model/model';
+
+import stripIndent = require('strip-indent');
+import {Warning, Severity} from 'polymer-analyzer/lib/warning/warning';
+import {stripWhitespace} from '../util';
+import {Rule} from '../rule';
+import {registry} from '../registry';
+
+export class UnknownEventHandlerDeclarations extends Rule {
+  code = 'unknown-event-handler-declarations';
+  description = stripIndent(`
+      Warns for unknown event handler declarations:
+
+          <paper-button on-unknown-event="foo">
+
+      Correct syntax:
+
+          <paper-button on-known-event="foo">
+  `).trim();
+
+  constructor() {
+    super();
+  }
+
+  async check(document: Document) {
+    const warnings: Warning[] = [];
+    const elements = document.getByKind('element-reference');
+    const toCamel = (name: string) =>
+        name.replace(/-(\w)/g, (matches) => matches[1].toUpperCase());
+
+    for (const element of elements) {
+      const definition = document.getOnlyAtId(
+          'polymer-element',
+          element.tagName,
+          {imported: true, externalPackages: true});
+
+      if (definition) {
+        const events = definition.events.map((e) => e.name);
+
+        for (const attr of element.attributes) {
+          const eventName = toCamel(attr.name.substr(3));
+          if (attr.name.indexOf('on-') === 0 &&
+              events.indexOf(eventName) === -1) {
+            warnings.push({
+              code: this.code,
+              message: stripWhitespace(`
+                  Unknown event handler "${attr.name}" on tag`),
+              severity: Severity.WARNING,
+              sourceRange: attr.sourceRange
+            });
+          }
+        }
+      }
+    }
+
+    return warnings;
+  }
+}
+
+registry.register(new UnknownEventHandlerDeclarations());
---
+++ src/test/polymer/unknown-event-handler-declarations_test.ts
@@ -0,0 +1,47 @@
+/**
+ * @license
+ * Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at
+ * http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at
+ * http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+import * as assert from 'assert';
+import * as path from 'path';
+import {Analyzer} from 'polymer-analyzer';
+import {FSUrlLoader} from 'polymer-analyzer/lib/url-loader/fs-url-loader';
+
+import {Linter} from '../../linter';
+import {UnknownEventHandlerDeclarations} from '../../polymer/unknown-event-handler-declarations';
+import {WarningPrettyPrinter} from '../util';
+
+const fixtures_dir = path.resolve(path.join(__dirname, '../../../test'));
+
+suite('UnknownEventHandlerDeclarations', () => {
+  let analyzer: Analyzer;
+  let warningPrinter: WarningPrettyPrinter;
+  let linter: Linter;
+
+  setup(() => {
+    analyzer = new Analyzer({urlLoader: new FSUrlLoader(fixtures_dir)});
+    warningPrinter = new WarningPrettyPrinter(analyzer);
+    linter = new Linter([new UnknownEventHandlerDeclarations()], analyzer);
+  });
+
+  test('finds unknown event handler declarations', async() => {
+    const warnings = await linter.lint([
+      'unknown-event-handler-declarations/unknown-event-handler-declarations.html'
+    ]);
+
+    assert.deepEqual(await warningPrinter.prettyPrint(warnings), [`
+      on-non-existent="foo"
+      ~~~~~~~~~~~~~~~~~~~~~`]);
+    assert.equal(
+        warnings[0].message, 'unknown event handler "non-existent" on foo');
+  });
+});
