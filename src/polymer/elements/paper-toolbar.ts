d04915e5b16ca6b3064ffc59eabc3c99f7b2d10f
Russell Bicknell <bicknellr@google.com>
2017-11-01 12:27:56 -0700

    Lint rule to update paper-toolbar uses from v1 to v2.

---
+++ src/polymer/elements/paper-toolbar.ts
@@ -0,0 +1,112 @@
+/**
+ * @license
+ * Copyright (c) 2017 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at
+ * http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at
+ * http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at
+ * http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at
+ * http://polymer.github.io/PATENTS.txt
+ */
+
+import * as util from 'util';
+
+import * as dom5 from 'dom5';
+import stripIndent = require('strip-indent');
+
+import {registry} from '../../registry';
+import {HtmlRule} from '../../html/rule';
+import {ParsedHtmlDocument, Severity, Warning} from 'polymer-analyzer';
+import {FixableWarning} from '../../warning';
+
+class PaperToolbar extends HtmlRule {
+  code = 'paper-toolbar';
+  description = stripIndent(`
+    (description paper-toolbar)
+  `).trim();
+
+  async checkDocument(parsedDocument: ParsedHtmlDocument) {
+    const warnings: Warning[] = [];
+
+    const paperToolbars = dom5.nodeWalkAll(
+      parsedDocument.ast,
+      dom5.predicates.hasTagName('paper-toolbar'),
+      undefined,
+      dom5.childNodesIncludeTemplate
+    );
+
+    for (const paperToolbar of paperToolbars) {
+      for (const child of paperToolbar.childNodes) {
+        if (child.tagName !== undefined && !dom5.hasAttribute(child, 'slot')) {
+          const startTagSourceRange =
+            parsedDocument.sourceRangeForStartTag(child)!;
+          const warning = new FixableWarning({
+            parsedDocument,
+            code: this.code,
+            message: '<paper-toolbar> no longer has a default slot: this ' +
+              'element will not appear in the composed tree. Add `slot="top"` ' +
+              'to distribute to the same position as the previous default ' +
+              'content.',
+            severity: Severity.WARNING,
+            sourceRange: startTagSourceRange
+          });
+
+          const [startOffset, endOffset]
+            = parsedDocument.sourceRangeToOffsets(startTagSourceRange);
+
+          const startTagText =
+            parsedDocument.contents.slice(startOffset, endOffset);
+          const isSelfClosing = startTagText.endsWith('/>');
+
+          warning.fix = [{
+            range: startTagSourceRange,
+            replacementText: startTagText.slice(0, isSelfClosing ? -2 : -1) +
+              ` slot="top"` + (isSelfClosing ? '/' : '') + '>',
+          }];
+
+          warnings.push(warning);
+        }
+
+        if (child.nodeName === '#text' && child.value.trim() !== '') {
+          const textNodeSourceRange = parsedDocument.sourceRangeForNode(child)!;
+
+          const warning = new FixableWarning({
+            parsedDocument,
+            code: this.code,
+            message: '<paper-toolbar> no longer has a default slot: this ' +
+              'text node will not appear in the composed tree. Wrap with ' +
+              '`<span slot="top">...</span>` to distribute to the same ' +
+              'position as the previous default content.',
+            severity: Severity.WARNING,
+            sourceRange: textNodeSourceRange
+          });
+
+          const fullText = child.value;
+          const trimmedText = fullText.trim();
+          const trimmedOffset = fullText.indexOf(trimmedText);
+
+          const replacementText =
+            fullText.substring(0, trimmedOffset) +
+            '<span slot="top">' +
+            trimmedText +
+            '</span>' +
+            fullText.substring(trimmedOffset + trimmedText.length);
+
+          warning.fix = [{
+            range: textNodeSourceRange,
+            replacementText,
+          }];
+
+          warnings.push(warning);
+        }
+      }
+    }
+
+    return warnings;
+  }
+}
+
+registry.register(new PaperToolbar());
