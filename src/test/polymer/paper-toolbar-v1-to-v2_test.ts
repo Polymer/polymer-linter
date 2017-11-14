650a2a22e9499297ff83bffdbfa925bb3c6a4225
Russell Bicknell <bicknellr@google.com>
2017-11-14 15:56:14 -0800

    Move the main chunk of the tests into a separate function since they all follow the same format.

--- src/test/polymer/paper-toolbar-v1-to-v2_test.ts
+++ src/test/polymer/paper-toolbar-v1-to-v2_test.ts
@@ -33,47 +33,41 @@ suite(ruleId, () => {
     linter = new Linter(registry.getRules([ruleId]), analyzer);
   });
 
+  async function assertFileChanges(inputFile: string, outputFile: string) {
+    const warnings = await linter.lint([inputFile]);
+    const edits = warnings.filter((w) => w.fix).map((w) => w.fix!);
+    const loader = makeParseLoader(analyzer);
+    const result = await applyEdits(edits, loader);
+    const inputFileContent = result.editedFiles.get(inputFile)
+    const outputFileContent = (await loader(outputFile)).contents;
+    assert.deepEqual(inputFileContent, outputFileContent);
+  }
+
   test('adds `slot="top"` to child elements without slots or special classes',
-    async() => {
-      const warnings = await linter.lint([`${ruleId}/child-default-slot_before.html`]);
-      const edits = warnings.filter((w) => w.fix).map((w) => w.fix!);
-      const loader = makeParseLoader(analyzer);
-      const result = await applyEdits(edits, loader);
-      assert.deepEqual(
-          result.editedFiles.get(`${ruleId}/child-default-slot_before.html`),
-          (await loader(`${ruleId}/child-default-slot_after.html`)).contents);
-    });
+      async() => {
+        await assertFileChanges(
+          `${ruleId}/child-default-slot_before.html`,
+          `${ruleId}/child-default-slot_after.html`);
+      });
 
   test('adds `slot="middle"` to child elements with `class="middle"`',
-    async() => {
-      const warnings = await linter.lint([`${ruleId}/child-middle-slot_before.html`]);
-      const edits = warnings.filter((w) => w.fix).map((w) => w.fix!);
-      const loader = makeParseLoader(analyzer);
-      const result = await applyEdits(edits, loader);
-      assert.deepEqual(
-          result.editedFiles.get(`${ruleId}/child-middle-slot_before.html`),
-          (await loader(`${ruleId}/child-middle-slot_after.html`)).contents);
-    });
+      async() => {
+        await assertFileChanges(
+          `${ruleId}/child-middle-slot_before.html`,
+          `${ruleId}/child-middle-slot_after.html`);
+      });
 
   test('adds `slot="bottom"` to child elements with `class="bottom"`',
-    async() => {
-      const warnings = await linter.lint([`${ruleId}/child-bottom-slot_before.html`]);
-      const edits = warnings.filter((w) => w.fix).map((w) => w.fix!);
-      const loader = makeParseLoader(analyzer);
-      const result = await applyEdits(edits, loader);
-      assert.deepEqual(
-          result.editedFiles.get(`${ruleId}/child-bottom-slot_before.html`),
-          (await loader(`${ruleId}/child-bottom-slot_after.html`)).contents);
-    });
+      async() => {
+        await assertFileChanges(
+          `${ruleId}/child-bottom-slot_before.html`,
+          `${ruleId}/child-bottom-slot_after.html`);
+      });
 
-    test('wraps non-whitespace child text nodes with `<span slot="top">`...' +
+  test('wraps non-whitespace child text nodes with `<span slot="top">`...' +
       '`</span>`', async() => {
-        const warnings = await linter.lint([`${ruleId}/child-non-whitespace-text_before.html`]);
-        const edits = warnings.filter((w) => w.fix).map((w) => w.fix!);
-        const loader = makeParseLoader(analyzer);
-        const result = await applyEdits(edits, loader);
-        assert.deepEqual(
-            result.editedFiles.get(`${ruleId}/child-non-whitespace-text_before.html`),
-            (await loader(`${ruleId}/child-non-whitespace-text_after.html`)).contents);
+        await assertFileChanges(
+          `${ruleId}/child-non-whitespace-text_before.html`,
+          `${ruleId}/child-non-whitespace-text_after.html`);
       });
 });
