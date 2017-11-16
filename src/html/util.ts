8f7639b57462616f4c435d906faca1c94e43f05e
valdrinkoshi <valdrin@google.com>
2017-11-15 18:15:49 -0800

    [skip ci] document attributeAliases

--- src/html/util.ts
+++ src/html/util.ts
@@ -181,6 +181,9 @@ export interface AttributeAliases { [s: string]: string; }
  * This is intended for handling only selectors that match an individual element
  * in isolation, it does throws if the selector talks about relationships
  * between elements like `.foo .bar` or `.foo > .bar`.
+ *
+ * Pass `attributeAliases` to apply selectors in non-standard attributes, e.g.
+ * <div class$="foo bar"></div>
  */
 export function elementSelectorToPredicate(
     simpleSelector: string,
