b6e8a25f102be6c8086d85a4279f86875af05218
valdrinkoshi <valdrin@google.com>
2017-12-13 12:10:42 -0800

    handle css-imports

--- src/polymer/elements/paper-button-style.ts
+++ src/polymer/elements/paper-button-style.ts
@@ -20,9 +20,17 @@ import {HtmlRule} from '../../html/rule';
 import {getIndentationInside, prependContentInto} from '../../html/util';
 import {registry} from '../../registry';
 import {stripIndentation} from '../../util';
-import {deepQuery} from './utils';
 
 const p = dom5.predicates;
+
+const outsideStyle = p.AND(
+    p.hasTagName('link'),
+    p.hasAttrValue('rel', 'import'),
+    p.hasAttrValue('type', 'css'));
+
+const setsDisplayInMixin = (styleDoc: Document) =>
+    /--paper-button:\s?{[^}]*display:[^}]*}/.test(
+        styleDoc.parsedDocument.contents);
 
 const cssRule = `<style id="linter-paper-button-style">
   /**
@@ -36,6 +44,14 @@ const cssRule = `<style id="linter-paper-button-style">
     text-align: center;
   }
 </style>`;
+
+const linkRule = `<!--
+ This style preserves the styling previous to
+ https://github.com/PolymerElements/paper-button/pull/115
+ This change can break the layout of paper-button content.
+ Remove this style to apply the change, more details at b/70528356.
+-->
+<link id="linter-paper-button-style" rel="import" type="css" href="data:text/css;charset=utf-8,${encodeURIComponent('paper-button{display:inline-block;text-align:center}')}">`;
 
 class PaperButtonStyle extends HtmlRule {
   code = 'paper-button-style';
@@ -54,60 +70,88 @@ class PaperButtonStyle extends HtmlRule {
       parsedDocument: ParsedHtmlDocument, document: Document,
       warnings: Warning[]) {
     const styleDocs = [...document.getFeatures({kind: 'css-document'})];
+    const linkDocs = [...document.getFeatures({kind: 'css-import'})];
     for (const domModule of document.getFeatures({kind: 'dom-module'})) {
-      const misplacedStyle =
-          dom5.query(domModule.astNode, p.hasTagName('style'));
-      if (misplacedStyle) {
-        warnings.push(new Warning({
-          code: 'paper-button-style',
-          message:
-              `Style outside template. Run \`move-style-into-template\` rule.`,
-          parsedDocument,
-          severity: Severity.ERROR,
-          sourceRange: parsedDocument.sourceRangeForStartTag(misplacedStyle)!
-        }));
-        continue;
-      }
-      const template = dom5.query(domModule.astNode, p.hasTagName('template'));
-      if (!template) {
-        continue;
-      }
-      const templateContent = treeAdapters.default.getTemplateContent(template);
+      // Already parsed.
       if (dom5.query(
-              templateContent,
-              p.hasAttrValue('id', 'linter-paper-button-style'))) {
+              domModule.astNode,
+              p.hasAttrValue('id', 'linter-paper-button-style'),
+              dom5.childNodesIncludeTemplate)) {
         continue;
       }
-      const buttonNode = deepQuery(templateContent, 'paper-button');
+      // paper-button not used.
+      const buttonNode = dom5.query(
+          domModule.astNode,
+          p.hasTagName('paper-button'),
+          dom5.childNodesIncludeTemplate);
       if (!buttonNode) {
         continue;
       }
-      const styleNode = dom5.query(
-          templateContent, p.AND(p.hasTagName('style'), (styleNode) => {
-            const i = styleDocs.findIndex((doc) => doc.astNode === styleNode);
+
+      let linkNode: dom5.Node | null = null;
+      const linkNodeUsingMixin = dom5.query(
+          domModule.astNode, p.AND(outsideStyle, (node: dom5.Node) => {
+            if (!linkNode) {
+              linkNode = node;
+            }
+            const i = linkDocs.findIndex((doc) => doc.astNode === node);
             if (i === -1) {
               return false;
             }
-            // Remove style from the docs to speed up next queries.
-            const styleDoc = styleDocs.splice(i, 1)[0];
-            return /--paper-button:\s?{[^}]*display:[^}]*}/.test(
-                styleDoc.parsedDocument.contents);
+            // Remove link from the docs to speed up next queries.
+            return [
+              ...linkDocs.splice(i, 1)[0].document.getFeatures(
+                  {kind: 'css-document'})
+            ].some(setsDisplayInMixin);
           }));
-      if (styleNode) {
+      if (linkNodeUsingMixin) {
         continue;
       }
+      if (linkNode) {
+        const indent =
+            getIndentationInside(domModule.astNode);
+        const insertion = `\n${indent}${linkRule.replace(/\n/g, '\n' + indent)}`;
+        warnings.push(new Warning({
+          code: 'paper-button-style',
+          message:
+              `paper-button v2 changed its style from \`display: inline-block\` to \`display: inline-flex\`.`,
+          parsedDocument,
+          severity: Severity.WARNING,
+          sourceRange: parsedDocument.sourceRangeForNode(buttonNode)!,
+          fix: [prependContentInto(
+              parsedDocument, domModule.astNode, insertion)]
+        }));
+      } else {
+        const template = dom5.query(domModule.astNode, p.hasTagName('template'))!;
+        const templateContent = treeAdapters.default.getTemplateContent(template);
 
-      const indent = getIndentationInside(templateContent);
-      const insertion = `\n${indent}${cssRule.replace(/\n/g, '\n' + indent)}`;
-      warnings.push(new Warning({
-        code: 'paper-button-style',
-        message:
-            `paper-button v2 changed its style from \`display: inline-block\` to \`display: inline-flex\`.`,
-        parsedDocument,
-        severity: Severity.WARNING,
-        sourceRange: parsedDocument.sourceRangeForNode(buttonNode)!,
-        fix: [prependContentInto(parsedDocument, template, insertion)]
-      }));
+        const styleNodeUsingMixin =
+            dom5.query(templateContent, (node: dom5.Node) => {
+              const i = styleDocs.findIndex((doc) => doc.astNode === node);
+              if (i === -1) {
+                return false;
+              }
+              // Remove style from the docs to speed up next queries.
+              return setsDisplayInMixin(styleDocs.splice(i, 1)[0]);
+            });
+        if (styleNodeUsingMixin) {
+          continue;
+        }
+
+        const indent =
+            getIndentationInside(templateContent);
+        const insertion = `\n${indent}${cssRule.replace(/\n/g, '\n' + indent)}`;
+        warnings.push(new Warning({
+          code: 'paper-button-style',
+          message:
+              `paper-button v2 changed its style from \`display: inline-block\` to \`display: inline-flex\`.`,
+          parsedDocument,
+          severity: Severity.WARNING,
+          sourceRange: parsedDocument.sourceRangeForNode(buttonNode)!,
+          fix: [prependContentInto(
+              parsedDocument, template, insertion)]
+        }));
+      }
     }
   }
 }
--- src/polymer/elements/utils.ts
+++ src/polymer/elements/utils.ts
@@ -24,20 +24,3 @@ export const nodeIsTemplateExtension = (node: parse5.ASTNode) => {
       node.tagName === 'template' && isAttrValue &&
       templateExtensionNames.includes(isAttrValue));
 };
-
-export const deepQuery =
-    (rootNode: dom5.Node, selector: string): dom5.Node | null => {
-      let node = dom5.query(rootNode, dom5.predicates.hasTagName(selector));
-      if (!node) {
-        const templates =
-            dom5.queryAll(rootNode, dom5.predicates.hasTagName('template'));
-        for (const template of templates) {
-          const templateContent =
-              parse5.treeAdapters.default.getTemplateContent(template);
-          node = deepQuery(templateContent, selector);
-          if (node)
-            break;
-        }
-      }
-      return node;
-    };
